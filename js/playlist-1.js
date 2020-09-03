8af0809c2dd491c4c6df8a87e788e3965c83336c
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-03 14:16:19 -0700

    add a function in playlist-1 to loop through the playlist array.

--- js/playlist-1.js
+++ js/playlist-1.js
@@ -6,3 +6,11 @@ const playlist = [
   'Three Little Birds',
   'The Way You Look Tonight'
 ];
+
+function createListItems(arr) { //The parameter arr is like a placeholder for an array that will be passed into this function
+  let items = '';
+  for ( let i = 0; i < arr.length; i ++ ) {
+    items += `<li>${ arr[i] }</li>`;
+  }
+  return items;
+}
